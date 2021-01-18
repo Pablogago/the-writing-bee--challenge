a51b9238d467dcd08ab35be8a57c9ccb14ddadb5
Pablo García <pablogarciabasolo@gmail.com>
2021-01-18 16:29:52 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './images/menu-logo.png';
+import './styles/App.css';
+
+function App() {
+  return (
+    <main className="app">
+      <header className="app-header">
+        <img src={logo} alt="The writing bee logo"/>
+        <nav>
+
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
